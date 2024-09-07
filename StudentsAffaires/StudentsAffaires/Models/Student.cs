b730cc150e2bf5efe329d319380255cc3e1cda03
Dr.Vista™ <Dr.Vista@DESKTOP-G0RAFD9>
2024-09-07 07:16:29 +0300

    Add project files.

---
+++ StudentsAffaires/StudentsAffaires/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAppInnotech;
+
+public class Student
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Mobile { get; set; }
+    public string? Telephone { get; set; }
+    public string? Email { get; set; }
+    public int Age { get; set; }
+    //public string? Message { get; set; }
+}
---
+++ StudentsAffaires/StudentsAffaires/Validators/StudentValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorAppInnotech.Validators
+{
+    public class StudentValidator : AbstractValidator<Student>
+    {
+        public StudentValidator()
+        {
+            RuleFor(student => student.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
+
+            RuleFor(student => student.Mobile)
+                .NotEmpty().WithMessage("Mobile number is required.")
+                .Matches(@"^\+?\d{10,15}$").WithMessage("Invalid mobile number format.");
+
+            RuleFor(student => student.Telephone)
+                .NotEmpty().WithMessage("Telephone number is required.")
+                .Matches(@"^\+?\d{10,15}$").WithMessage("Invalid telephone number format.");
+
+            RuleFor(student => student.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email address.");
+
+            RuleFor(student => student.Age)
+                .InclusiveBetween(1, 100);
+
+            //RuleFor(student => student.Message)
+            //    .NotEmpty().WithMessage("Message is required.")
+            //    .MaximumLength(140).WithMessage("Message cannot exceed 140 characters.");
+        }
+    }
+}
