cef2d3a71741061f70b3bea3b6e33d2d9bb66b63
sickoovit <omarnasser665@gmail.com>
2024-09-09 15:00:28 +0300

    Setup DbContext using user-secrets

--- StudentsAffairs.Server/Program.cs
+++ StudentsAffairs.Server/Program.cs
@@ -6,6 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Configuration.AddUserSecrets<Program>();
+
+string connectionStringUserDbConfigurationKey = "UserDbConnectionString";
+string connectionStringUserDb = builder.Configuration[connectionStringUserDbConfigurationKey] ??
+    throw new ArgumentNullException(
+        paramName: "connectionStringUserDbConfigurationKey",
+        message: "Connection String Couldn't Be Resolved. Configuration Key may be not valid"
+    );
+
+string connectionStringAssignmentDbConfigurationKey = "AssignmentDbConnectionString";
+string connectionStringAssignmentDb = builder.Configuration[connectionStringAssignmentDbConfigurationKey] ??
+    throw new ArgumentNullException(
+        paramName: "connectionStringAssignmentDbConfigurationKey",
+        message: "Connection String Couldn't Be Resolved. Configuration Key may be not valid"
+    );
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlServer(connectionStringUserDb));
+
+builder.Services.AddDbContext<AssignmentDbContext>(options =>
+    options.UseSqlServer(connectionStringAssignmentDb));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
