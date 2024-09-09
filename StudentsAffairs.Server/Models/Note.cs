780dde051e3ea9fa0171d82f10c8d5c0f9b1bf51
sickoovit <omarnasser665@gmail.com>
2024-09-09 14:52:42 +0300

    Add Models

---
+++ StudentsAffairs.Server/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentsAffairs.Models;
+
+public class Note
+{
+    // Unique identifier for the note.
+    public int Id { get; set; }
+    // The content or text of the note.
+    public string? Content { get; set; }
+    // Date and time when the note was created.
+    public DateTime CreatedDate { get; set; }
+    // Last updated date and time for the note, nullable if the note hasn’t been updated.
+    public DateTime? LastUpdatedDate { get; set; }
+    // Foreign key to associate the note with a specific course (optional).
+    public int? CourseId { get; set; }
+    // Foreign key to associate the note with a specific lesson (optional).
+    public int? LessonId { get; set; }
+    // Flag indicating whether the note is marked as important.
+    public bool IsImportant { get; set; }
+}
---
+++ StudentsAffairs.Server/Models/Tutor.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentsAffairs.Models;
+
+public class Tutor : User
+{
+    // List of courses the tutor is teaching.
+    public List<Course>? Courses { get; set; }
+
+    // List of students the tutor is mentoring.
+    public List<Student>? Students { get; set; }
+
+    // Specialty or area of expertise of the tutor.
+    public string? Specialty { get; set; }
+}
