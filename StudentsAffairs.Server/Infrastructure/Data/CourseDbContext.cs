2ee231fbd80b21cfbeff17327e9c5d625d7d3098
sickoovit <omarnasser665@gmail.com>
2024-09-10 15:33:51 +0300

    create course dbcontext

---
+++ StudentsAffairs.Server/Infrastructure/Data/CourseDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentsAffairs.Server.Infrastructure.Data;
+public class CourseDbContext : DbContext
+{
+    public CourseDbContext(DbContextOptions<CourseDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Lecture> Lectures { get; set; }
+
+    // Other DbSets...
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure Course-Lecture relationship
+        modelBuilder.Entity<Lecture>()
+            .HasOne(l => l.Course)
+            .WithMany(c => c.Lectures)
+            .HasForeignKey(l => l.CourseId);
+
+        // Other model configurations...
+    }
+}
