64fa858d918222e7f15cd510b50113066abe6321
sickoovit <omarnasser665@gmail.com>
2024-09-10 16:11:28 +0300

    create ILectureRepository

---
+++ StudentsAffairs.Server/Domain/Interfaces/ILectureRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentsAffairs.Server.Domain.Interfaces;
+
+public interface ILectureRepository : IRepository<Lecture>
+{
+    Task<IEnumerable<Lecture>> GetLecturesByCourseId(int courseId);
+    Task<IEnumerable<Lecture>> GetLecturesByScheduledDate(DateTime scheduledDate);
+    
+}
