3167003bcb72f3e7f9e9bef6afd582298dd04327
sickoovit <omarnasser665@gmail.com>
2024-09-10 18:51:08 +0300

    use interfaces, use repositories

--- StudentsAffairs.Server/GlobalUsings.cs
+++ StudentsAffairs.Server/GlobalUsings.cs
@@ -2,4 +2,6 @@
 global using FluentValidation;
 
 global using StudentsAffairs.Server.Domain.Entities;
+global using StudentsAffairs.Server.Domain.Interfaces;
 global using StudentsAffairs.Server.Infrastructure.Data;
+global using StudentsAffairs.Server.Infrastructure.Repositories;
