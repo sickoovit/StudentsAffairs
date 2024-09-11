902a71ac315273ebb3ace32246a2b314bd298b01
sickoovit <omarnasser665@gmail.com>
2024-09-11 04:10:40 +0300

    create lecture repo

---
+++ StudentsAffairs.Server/Infrastructure/Repositories/LectureRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentsAffairs.Server.Infrastructure.Repositories;
+
+public class LectureRepository : Repository<Lecture, CourseDbContext>, ILectureRepository
+{
+    public LectureRepository(CourseDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Lecture>> GetLecturesByCourseId(int courseId)
+    {
+        return await _context.Lectures
+            .Where(l => l.CourseId == courseId)
+            .ToListAsync();
+    }
+
+    // Get all lectures by ScheduledDate asynchronously
+    public async Task<IEnumerable<Lecture>> GetLecturesByScheduledDate(DateTime scheduledDate)
+    {
+        return await _context.Lectures
+            .Where(l => l.ScheduledDate.Date == scheduledDate.Date)
+            .ToListAsync();
+    }
+}
