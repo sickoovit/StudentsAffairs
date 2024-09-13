fff24ad14b1d07170283302dbbc28bf35c208f64
sickoovit <omarnasser665@gmail.com>
2024-09-13 05:03:51 +0300

    add cache service and interface

---
+++ StudentsAffairs.Server/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentsAffairs.Server;
+
+public interface ICacheService
+{
+    T GetOrAdd<T>(string key, Func<T> fetchFunction);
+    Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> fetchFunction);
+    void ClearCache(string key);
+    void ClearAll();
+}
