4bc547b30f08b6147a3bc8644261a2431bdf850d
sickoovit <omarnasser665@gmail.com>
2024-09-13 05:04:08 +0300

    add usings

--- StudentsAffairs.Server/GlobalUsings.cs
+++ StudentsAffairs.Server/GlobalUsings.cs
@@ -1,6 +1,9 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using FluentValidation;
 
+global using Microsoft.AspNetCore.Components;
+
+global using StudentsAffairs.Server;
 global using StudentsAffairs.Server.Domain.Entities;
 global using StudentsAffairs.Server.Domain.Interfaces;
 global using StudentsAffairs.Server.Infrastructure.Data;
