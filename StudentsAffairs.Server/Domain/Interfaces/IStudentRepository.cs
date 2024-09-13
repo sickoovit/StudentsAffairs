d2ed831169565a481e1b3cf3408994d1d86445e1
sickoovit <omarnasser665@gmail.com>
2024-09-13 05:01:08 +0300

    create usertypes repositories

---
+++ StudentsAffairs.Server/Domain/Interfaces/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentsAffairs.Server.Domain.Interfaces;
+
+public interface IStudentRepository
+{
+    Task<IEnumerable<Student>> GetAllStudentsAsync();
+    Task<Student?> GetStudentByIdAsync(Guid studentId);
+    Task AddStudentAsync(Student student);
+    Task UpdateStudentAsync(Student student);
+    Task DeleteStudentAsync(Guid studentId);
+    Task<IEnumerable<Student>> GetStudentsByCourseAsync(Guid courseId);
+}
