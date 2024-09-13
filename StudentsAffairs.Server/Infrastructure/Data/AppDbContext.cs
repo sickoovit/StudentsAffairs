2d2e992d862c5673b78e61b4787710bad1764db0
sickoovit <omarnasser665@gmail.com>
2024-09-13 05:01:44 +0300

    merge dbcontexts into appDbContext

---
+++ StudentsAffairs.Server/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,82 @@
+﻿namespace StudentsAffairs.Server.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Assignment> Assignments { get; set; }
+    public DbSet<AssignmentSubmission> AssignmentSubmissions { get; set; }
+
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Lecture> Lectures { get; set; }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Tutor> Tutors { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        UserModelConfiguration(modelBuilder);
+        LectureModelConfiguration(modelBuilder);
+        AssignmentModelConfiguration(modelBuilder);
+        AssignmentSubmissionModelConfiguration(modelBuilder);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected void UserModelConfiguration(ModelBuilder modelBuilder)
+    {
+        // Configure TPH inheritance for the User class
+        modelBuilder.Entity<User>()
+            .HasDiscriminator<string>("UserType")
+            .HasValue<Student>("Student")
+            .HasValue<Tutor>("Tutor")
+            .HasValue<Admin>("Admin");
+
+    }
+
+    protected void LectureModelConfiguration(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Lecture>()
+            .HasOne(l => l.Course)
+            .WithMany(c => c.Lectures)
+            .HasForeignKey(l => l.CourseId);
+    }
+
+    protected void AssignmentModelConfiguration(ModelBuilder modelBuilder)
+    {
+        // Configure Assignment
+        modelBuilder.Entity<Assignment>()
+            .HasOne(a => a.Course)
+            .WithMany(c => c.Assignments)
+            .HasForeignKey(a => a.CourseId);
+
+        // Configure AssignmentSubmission
+        modelBuilder.Entity<AssignmentSubmission>()
+            .HasOne(s => s.Assignment)
+            .WithMany(a => a.Submissions)
+            .HasForeignKey(s => s.AssignmentId);
+
+        modelBuilder.Entity<AssignmentSubmission>()
+            .HasOne(s => s.Student)
+            .WithMany()
+            .HasForeignKey(s => s.StudentId);
+    }
+
+    protected void AssignmentSubmissionModelConfiguration(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AssignmentSubmission>()
+            .HasOne(s => s.Assignment)
+            .WithMany(a => a.Submissions)
+            .HasForeignKey(s => s.AssignmentId);
+
+        modelBuilder.Entity<AssignmentSubmission>()
+            .HasOne(s => s.Student)
+            .WithMany()
+            .HasForeignKey(s => s.StudentId);
+    }
+}
