eeb981594c6700abb07ee2b3616a4404499fed8a
sickoovit <omarnasser665@gmail.com>
2024-09-13 05:01:26 +0300

    bugfix id to guid

--- StudentsAffairs.Server/Domain/Interfaces/ILectureRepository.cs
+++ StudentsAffairs.Server/Domain/Interfaces/ILectureRepository.cs
@@ -2,7 +2,7 @@
 
 public interface ILectureRepository : IRepository<Lecture>
 {
-    Task<IEnumerable<Lecture>> GetLecturesByCourseId(int courseId);
+    Task<IEnumerable<Lecture>> GetLecturesByCourseId(Guid courseId);
     Task<IEnumerable<Lecture>> GetLecturesByScheduledDate(DateTime scheduledDate);
     
 }
