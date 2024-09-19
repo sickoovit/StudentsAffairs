0ce65c269564c043f51b74fd7ee24971af60f5cf
sickoovit <omarnasser665@gmail.com>
2024-09-19 16:33:11 +0300

    add global usings

--- StudentsAffairs.Server/GlobalUsings.cs
+++ StudentsAffairs.Server/GlobalUsings.cs
@@ -1,10 +1,8 @@
 ﻿global using Microsoft.EntityFrameworkCore;
+global using Blazored.SessionStorage;
 global using FluentValidation;
 
 global using Microsoft.AspNetCore.Components;
 
 global using StudentsAffairs.Server;
-global using StudentsAffairs.Server.Domain.Entities;
-global using StudentsAffairs.Server.Domain.Interfaces;
-global using StudentsAffairs.Server.Infrastructure.Data;
-global using StudentsAffairs.Server.Infrastructure.Repositories;
+global using StudentsAffairs.Server.Components;
