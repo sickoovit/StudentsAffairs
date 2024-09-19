f3217cbf78510fb56967ed2a99a873039ff11f61
sickoovit <omarnasser665@gmail.com>
2024-09-19 16:33:21 +0300

    move to class library

--- StudentsAffairs.Server/ICacheService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentsAffairs.Server;
-
-public interface ICacheService
-{
-    T GetOrAdd<T>(string key, Func<T> fetchFunction);
-    Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> fetchFunction);
-    void ClearCache(string key);
-    void ClearAll();
-}
