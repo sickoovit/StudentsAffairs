ffc13000a764be3d6797c5944090500508baf4af
sickoovit <omarnasser665@gmail.com>
2025-03-07 09:07:52 +0200

    add redis helper

---
+++ StudentsAffairs.Redis/Helpers/DistributedCacheExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentsAffairs.Redis.Helpers
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(
+            this IDistributedCache cache,
+            string key,
+            T data,
+            TimeSpan expiration)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiration
+            };
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(key, jsonData, options);
+        }
+
+        public static async Task<T?> GetRecordAsync<T>(
+            this IDistributedCache cache,
+            string key)
+        {
+            var jsonData = await cache.GetStringAsync(key);
+            return jsonData == null ? default : JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+
+}
